d44257a975e0961c87f90dc3bdf8e9ace7784fa2
TanNX072906 <hahachenal@gmail.com>
2026-02-28 14:11:56 +0700

    update InvoiceService

--- src/java/service/InvoiceService.java
+++ src/java/service/InvoiceService.java
@@ -1,6 +1,11 @@
 package service;
 
 import entity.Invoice;
+import entity.Shift;
+import entity.User;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import repository.ShiftDAO;
 
 /**
  *
@@ -12,7 +17,18 @@ public class InvoiceService {
      * Holding the CRUD function of Invoice
      */
     
-    public void CreateInvoice(Invoice invoice){
+    public void CreateInvoice(Invoice invoice) throws Exception{
+        //khi create can truyen vao InvoiceCode, amount, status, createBy
+        if(invoice.getAmount().compareTo(new BigDecimal(0)) <=0){
+            throw new Exception("Amount must be larger than 0");
+        }
+        invoice.setStatus("APPROVED");
         
+//        if(invoice.getAmount().compareTo(new BigDecimal(5000000)) >= 0){
+//            
+//        }
+//         Call alertDAO to make an outlier alert
+
+
     }
 }

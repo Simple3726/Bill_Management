310826dbeb1e5956030cc07822802ad888b51606
TanNX072906 <hahachenal@gmail.com>
2026-03-01 18:18:50 +0700

    Thêm điều phối dành cho list thông qua InvoiceController

--- src/java/controller/InvoiceController.java
+++ src/java/controller/InvoiceController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import service.InvoiceService;
 
 /**
  *
@@ -25,23 +26,41 @@ public class InvoiceController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet InvoiceController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet InvoiceController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    
+    private InvoiceService service;
+    
+    @Override
+    public void init() throws ServletException {
+        service = new InvoiceService();
     }
-
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    
+    String action = request.getPathInfo();
+    
+    // Nếu action là null hoặc chỉ có "/" thì mặc định là List
+    if (action == null || action.equals("/")) {
+        action = "/List";
+    }
+    
+    // Sử dụng switch-case với dấu gạch chéo
+    switch (action) {
+        case "/List":
+            listInvoice(request, response);
+            break; // Đừng quên break để tránh trôi code
+        default:
+            // Nên có một trang 404 hoặc báo lỗi ở đây thay vì để trắng
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            break;
+    }
+}
+    
+    private void listInvoice(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.setAttribute("invoiceList", service.getAllInvoice());
+        request.getRequestDispatcher("/WEB-INF/invoice_list.jsp").forward(request, response);
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.

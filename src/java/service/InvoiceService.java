bbbbb38b2741f6ae1fbf1906463cb60d062a1c53
TanNX072906 <hahachenal@gmail.com>
2026-03-07 12:23:29 +0700

    Revert "Merge branch 'master' of https://github.com/Simple3726/Bill_Management"
    This reverts commit 1ae04197a876e28744db264efc6445abe6082586, reversing
    changes made to 1ca87f6226bda5f75d42a0deb85e35fdec57b157.

--- src/java/service/InvoiceService.java
+++ src/java/service/InvoiceService.java
@@ -158,7 +158,7 @@ public class InvoiceService {
                 "APPROVE_INVOICE",
                 "INVOICE",
                 invoiceId,
-                "Invoice approved"
+                LocalDateTime.now()
         );
 
         // =========================
